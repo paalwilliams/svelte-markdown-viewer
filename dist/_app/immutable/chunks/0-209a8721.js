e502f9ea33af1cee387532de9f81327c4aa7f36e
Paal <paal@paals-mbp-2.localdomain>
2022-09-20 22:34:58 +0200

    remove dist folder

--- dist/_app/immutable/chunks/0-209a8721.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/layout.svelte-b04e9434.js";export{t as component};
--- dist/_app/immutable/chunks/1-b4a466b4.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-bc7922fe.js";export{t as component};
